db9495a264bee6fbb1f8a098445cb60b56184656
AkshithaReddy005 <akshithareddy2405@gmail.com>
2025-06-28 14:27:49 +0530

    Add tourist attractions finder using geolocation + Overpass API

---
+++ pages/tourist-places.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TouristPlacesSection from '../src/components/sections/TouristPlacesSection';
+
+const TouristPlacesPage = () => {
+  return (
+    <div>
+      <TouristPlacesSection />
+    </div>
+  );
+};
+
+export default TouristPlacesPage;
