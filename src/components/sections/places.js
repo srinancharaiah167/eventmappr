b41122442a3956d4f2270f41fc794d5ec7ef0e24
Peehu Mishra <peehumishra1308@gmail.com>
2025-07-31 11:30:24 +0530

    Create places.js

---
+++ src/components/sections/places.js
@@ -0,0 +1,9 @@
+'use client';
+
+export default function Places(){
+    <div>
+        <div className="h-[20%] w-[10%]" style={{background:'green',borderRadius:24,}}>
+            <h1>hi this is me</h1>
+        </div>  
+    </div>
+}
